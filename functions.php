f8e43c1950eff18be946f0dbea5b5afc7b850e9c
Philrey <paderogaophilrey@gmail.com>
2017-10-30 10:36:00 -0800

    added uploading image functionality

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
+    //Connect to Database//
     $connect_to_db = null;
     function connect_to_database($host_name,$user_name,$pass,$db_name,$close_db){
         $connect_to_db = mysqli_connect($host_name,$user_name,$pass,$db_name);
@@ -14,8 +15,90 @@
             mysqli_close($connect_to_db);
         }
     }
-
+    //Search Items With//
     function search_query($column,$from,$where){
+        //2. Performing a query//
         $query = `SELECT {$column} FROM {$from} WHERE {$where}`;
+        $result = mysqli_query($connection,$query);
+        //Checks if there was no result
+        if(!$result){
+            die("Query Failed");
+        }else {
+            $cLine = array();
+            //3. Use Returned Rows //
+            while($row = mysqli_fetch_assoc($result)){
+                // Output each row//
+                                  
+                $cLine[] = array($row['id'],$row['user_id'],$row['movie_name'],$row['seat_number'],$row['booked_date']);
+            }
+            return $cLine;
+        }
+        
     }
 ?>
+<html>
+    <body>
+        <form method="post" enctype="multipart/form-data">
+            <br/>
+            <input type="file" name="image">
+            <br><br>
+            <input type="submit" value="Upload Image" name="submit">
+        </form>
+        <?php
+            display_image();
+
+            ini_set('mysql.connect_timeout',300);
+            ini_set('default_socket_timeout',300);
+            if(isset($_POST['submit'])){
+                if(getimagesize($_FILES['image']['tmp_name'])==FALSE){
+                    echo "Please Select an Image to Upload.";
+                }else{
+                    $image = addslashes($_FILES['image']['tmp_name']);
+                    $name = addslashes($_FILES['image']['name']);
+                    $image = file_get_contents($image);
+                    $image = base64_encode($image);
+                    save_image($name,$image);
+                }
+            }
+            function save_image($name,$image){
+                $connect_to_db = mysqli_connect('localhost','cinema_admin','admin','cinema_db');
+                $query = 'insert into movie_management ';
+                $query .="(movie_name,movie_price,movie_details,movie_image,image_name)VALUES";
+                $query .="('Phil Rey','150','The Secret Behind the Legend','" . $image . "','" . $name . "')";
+                echo $query . "<br/>";
+        
+                $result = mysqli_query($connect_to_db,$query);
+                if($result){
+                    echo "Image Uploaded";
+                    display_image();
+                }else{
+                    echo "Image not Uploaded";
+                }
+            }
+            function display_image(){
+                $connect_to_db = mysqli_connect('localhost','cinema_admin','admin','cinema_db');
+                $query = 'select * from movie_management';
+                //echo $query . "<br/>";
+
+                $result = mysqli_query($connect_to_db,$query);
+                while($row = mysqli_fetch_assoc($result)){
+                    // Output each row//
+                                      
+                    $cLine[] = array($row['movie_name'],$row['movie_price'],$row['movie_details'],$row['movie_image']);
+                }
+                mysqli_free_result($result);
+                for($n=0;$n<sizeof($cLine);$n++){
+                    for($x=0;$x<sizeof($cLine[$n]);$x++){
+                        if($x==3){
+                            echo '<img /*height="200" width="150"*/ src="data:image;base64,'.$cLine[$n][$x].'">';
+                        }else{
+                            echo $cLine[$n][$x];
+                        }
+                        echo ", ";
+                    }
+                    echo "<br/>";
+                }
+            }
+?>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -1,22 +1,5 @@
 <!DOCTYPE html>
 
 <html>
-    <?php
-        include_once("functions.php");
-        connect_to_database("localhost","cinema_admin","cinema_admin","cinema_db",true);
-    ?>
-    <?php 
-        $name = "next page";
-        $prep = "Phil_Rey_01 @ gmail.com";
-        $id = 2;
-    ?>
-    <a href="asda" ></a>
-    <title>Main Menu</title>
-    <form action="log_in.php" method="post" target="blank">
-        User Nime: <input type="text" name="userN" value="asd" maxlength="5"/><br>
-        Passwerd: <input type="password" name="passW" value="asd"/><br>
-        Number: <input type="number" name="numbeR" value ="asd"/><br>
-        Date: <input type="datetime" name="datE" /><br>
-        <input type="submit" name="submit" value="Submit"/>
-    </form>
+    
 </html>
