347d6a08bc613428515332b16458f6e70e7ac40d
Philrey <paderogaophilrey@gmail.com>
2017-11-08 06:47:52 -0800

    added header

---
+++ header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="Styles.css" type="text/css" rel="stylesheet" />
+        <title>Main Menu</title>
+    </head>
+    <body>
+        <!--Header-->
+        <table border = "1" width="" id="header">
+            asda
+            <tr>
+                <!--Logo-->
+                <td class="left" width="100" height="100">
+                    asda
+                    <img src="buttons/logo.png" alt="" width = "100">
+                </td>
+                <td class="left">
+                    SLEC Cinema
+                </td>
+                <!--Log In Button-->
+                <td class="right">
+                    <a href="" id="login"><img src="buttons/loginA.png" width="100"></div></a>
+                </td>
+                <!--Log Out Button-->
+                <td class="right">
+                    <a href="" id="login"><img src="buttons/logoutA.png" width="100"></div></a>
+                </td>
+                <td class="right">Manage Movies</td>
+            </tr>
+
+        </table>
+    </body>
+</html>
