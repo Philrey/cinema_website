61ea8e46b860710dbd81fc141a7b594e88738b3f
Philrey <paderogaophilrey@gmail.com>
2017-11-08 08:06:16 -0800

    added body

--- index.php
+++ index.php
@@ -1,5 +1,68 @@
 <!DOCTYPE html>
-
 <html>
-    
+    <head>
+        <link href="Styles.css" type="text/css" rel="stylesheet" />
+        <title>Main Menu</title>
+    </head>
+    <body>
+        <!--Header-->
+        <table id="header">
+            <tr>
+                <!--Logo-->
+                <td class="left" >
+                    <img src="buttons/logo.png" alt="" width = "50">
+                </td>
+                <td class="left">
+                    SLEC Cinema
+                </td>
+                <!--Log In Button-->
+                <td class="right">
+                    <a href="" id="buttons"><img src="buttons/loginA.png" width="100"></div></a>
+                </td>
+                <!--Log Out Button-->
+                <td class="right">
+                    <a href="" id="buttons"><img src="buttons/logoutA.png" width="100"></div></a>
+                </td>
+                <!--Manage Button-->
+                <td class="right">
+                    <a href="" id="buttons"> <img src="buttons/manageA.png" height="39"> </a>
+                </td>
+            </tr>
+        </table>
+        <table width="100%">
+            <tr>
+                <form action="" method="get">
+                    <td class="right">
+                        <input type="submit" value="Search" id="searchbutton">
+                    </td>
+                    <td class="right">
+                        <input type="text" name="tosearch" id="" size="30">
+                    </td>
+                </form>
+                
+            </tr>
+        </table>
+        <br>
+        <!--Use Php for display-->
+
+        <table border="2" width="100%">
+            <tr>
+                <!--Image Container-->
+                <td width="200" height="200">
+                    Image Here
+                </td>
+                <!--Info Container-->
+                <td>
+                    <p border="1">Title</p>
+                    <p border="1">Details</p>
+                    <p border="1">asd</p>
+                    <p border="1">asd</p>
+                    <p border="1">asd</p>
+                    <p border="1">asd</p>
+                </td>
+            </tr>
+        </table>
+
+        
+    </body>
 </html>
